f90c0fe836da932e2bab07b3aae828f878630d30
SystemGlitch <jeremy.la@outlook.fr>
2019-01-10 16:51:49 +0100

    Remove unused oauth routes + setup password grant

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Laravel\Passport\Passport;
+use Laravel\Passport\RouteRegistrar;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -25,7 +26,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        Passport::routes();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::delete('/logout', 'Auth\\ApiLoginController@logout');
 });
+
+Route::post('/login', 'Auth\\ApiLoginController@login');
