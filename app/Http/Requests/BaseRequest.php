2d281ede87fb741c1b114b99ef595c187bdcef58
SystemGlitch <jeremy.la@outlook.fr>
2019-03-25 14:43:11 +0100

    Always return json response

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BaseRequest extends FormRequest
+{
+
+    public function expectsJson()
+    {
+        return true;
+    }
+
+    public function wantsJson()
+    {
+        return true;
+    }
+}
