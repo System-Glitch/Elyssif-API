1e7bd8d4b7fb5244375463c6e9dcf3c9fd3f8ae1
SystemGlitch <jeremy.la@outlook.fr>
2019-04-30 19:31:02 +0200

    Add txid column

--- database/migrations/2019_04_30_133746_create_transactions_table.php
+++ database/migrations/2019_04_30_133746_create_transactions_table.php
@@ -20,6 +20,7 @@ class CreateTransactionsTable extends Migration
             ->references('id')
             ->on('files')
             ->onDelete('cascade');
+            $table->string('txid')->unique();
             $table->integer('confirmations')->unsigned()->default(0);
             $table->double('amount')->default(0);
             $table->timestamps();
