a4ae3828da0f806f3c17e2b8e22954de6d9117be
JeremySvn <svensson.jeremy94@gmail.com>
2019-05-15 23:10:59 +0200

    SendTransaction : removed unused imports

--- app/Jobs/SendTransaction.php
+++ app/Jobs/SendTransaction.php
@@ -2,11 +2,6 @@
 
 namespace App\Jobs;
 
-//use Illuminate\Bus\Queueable;
-//use Illuminate\Queue\SerializesModels;
-//use Illuminate\Queue\InteractsWithQueue;
-//use Illuminate\Contracts\Queue\ShouldQueue;
-//use Illuminate\Foundation\Bus\Dispatchable;
 use App\Models\Transaction;
 use App\Events\TransactionNotification;
 
