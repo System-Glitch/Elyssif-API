27a0d899ef925f7c22dc19c091ef8c7261e1413b
JeremySvn <svensson.jeremy94@gmail.com>
2019-05-16 18:01:50 +0200

    Update SendTransaction command
    Class name change to BitcoinSend
    Command call change to bitcoin:send
    Type boolean check on feesDeducted argument

---
+++ app/Console/Commands/BitcoinSend.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\SendToAddress;
+use Illuminate\Console\Command;
+
+class BitcoinSend extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bitcoin:send {address} {amount} {feesDeducted}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sending Bitcoin transaction on blockchain (\'sendtoaddress\')';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if(strtolower($this->argument('feesDeducted')) == "true" || strtolower($this->argument('feesDeducted')) == "false"){
+            $feesDeducted = filter_var($this->argument('feesDeducted'), FILTER_VALIDATE_BOOLEAN);
+            dispatch(new SendToAddress($this->argument('address'), $this->argument('amount'), $feesDeducted));
+        }else{
+            $this->output->writeln("Third argument given is no boolean (".$this->argument('feesDeducted').")");
+            return;
+        }
+    }
+}
