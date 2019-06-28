9af22659c0a912b40d56889fc1b24ea7c6c569a6
SystemGlitch <jeremy.la@outlook.fr>
2019-06-28 15:25:32 +0200

    Custom error pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<meta name="description" content="A secure file sharing software.">
-
-	<link rel="icon" type="image/png" href="{{ asset('img/logo16.png') }}" sizes="16x16">
-	<link rel="icon" type="image/png" href="{{ asset('img/logo32.png') }}" sizes="32x32">
-	<link rel="icon" type="image/png" href="{{ asset('img/logo64.png') }}" sizes="64x64">
-	<link rel="icon" type="image/png" href="{{ asset('img/logo128.png') }}" sizes="128x128">
-	<link rel="icon" type="image/png" href="{{ asset('img/logo256.png') }}" sizes="256x256">
-
-	<title>Elyssif</title>
-
-	<link rel="stylesheet" type="text/css" href="css/app.css">
-</head>
-<body>
+@extends('root')
+@section('title', 'Elyssif')
+@section('body')
 	<section class="hero is-fullheight gradient" id="welcome">
 		<div class="hero-body">
 			<div class="container has-text-centered">
@@ -118,7 +102,4 @@
 			</div>
 		</div>
 	</section>
-
-	<script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+@endsection
